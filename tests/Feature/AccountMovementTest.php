d625015c51b60f70a3d2be618f3156e4365ce54e
Jose Tlacuilo <tlacuilose@outlook.com>
2021-09-24 22:05:05 -0500

    Functional tests.

--- tests/Feature/AccountMovementTest.php
+++ tests/Feature/AccountMovementTest.php
@@ -34,4 +34,74 @@ class AccountMovementTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_same_transfer()
+    {
+        $user = User::factory()->create();
+        $source_acc = Account::create([
+            'name' => 'Source Test Account',
+            'current_balance' => 100,
+            'user_id' => $user->id
+        ]);
+        $dest_acc = Account::create([
+            'name' => 'Destination Test Account',
+            'current_balance' => 200,
+            'user_id' => $user->id
+        ]);
+        Account::transfer($source_acc, $dest_acc, 20);
+        $this->assertEquals(220, $dest_acc->current_balance);
+    }
+
+    public function test_same_transfer_movement()
+    {
+        $user = User::factory()->create();
+        $source_acc = Account::create([
+            'name' => 'Source Test Account',
+            'current_balance' => 100,
+            'user_id' => $user->id
+        ]);
+        $dest_acc = Account::create([
+            'name' => 'Destination Test Account',
+            'current_balance' => 200,
+            'user_id' => $user->id
+        ]);
+        $movements = Account::transfer($source_acc, $dest_acc, 20);
+        $this->assertEquals(Movement::TRANSFER, $movements[0]->type);
+    }
+
+    public function test_third_transfer()
+    {
+        $user = User::factory()->create();
+        $user2 = User::factory()->create();
+        $source_acc = Account::create([
+            'name' => 'Source Test Account',
+            'current_balance' => 100,
+            'user_id' => $user->id
+        ]);
+        $dest_acc = Account::create([
+            'name' => 'Destination Test Account',
+            'current_balance' => 200,
+            'user_id' => $user2->id
+        ]);
+        Account::transfer($source_acc, $dest_acc, 20);
+        $this->assertEquals(220, $dest_acc->current_balance);
+    }
+
+    public function test_third_transfer_movement()
+    {
+        $user = User::factory()->create();
+        $user2 = User::factory()->create();
+        $source_acc = Account::create([
+            'name' => 'Source Test Account',
+            'current_balance' => 100,
+            'user_id' => $user->id
+        ]);
+        $dest_acc = Account::create([
+            'name' => 'Destination Test Account',
+            'current_balance' => 200,
+            'user_id' => $user2->id
+        ]);
+        $movements  = Account::transfer($source_acc, $dest_acc, 20);
+        $this->assertEquals(20, $dest_acc->current_balance);
+    }
 }
--- tests/Unit/AccountMovementTest.php
+++ tests/Unit/AccountMovementTest.php
@@ -10,25 +10,38 @@ use App\Models\User;
 class AccountMovementTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
-     * A basic unit test example.
+     * Tests that the account can hold a balance.
      *
      * @return void
      */
-    public function test_firstparty_transfer()
+    public function test_correct_balance()
     {
+        $balance = 200;
         $user = User::factory()->create();
-        $source_acc = Account::create([
+        $account = Account::create([
             'name' => 'Source Test Account',
-            'current_balance' => 100,
+            'current_balance' => $balance,
             'user_id' => $user->id
         ]);
-        $dest_acc = Account::create([
-            'name' => 'Destination Test Account',
-            'current_balance' => 200,
+        $this->assertEquals($balance, $account->current_balance);
+    }
+
+    /**
+     * Tests that the account cannot hold a alphanumeric balance.
+     *
+     * @return void
+     */
+    public function test_incorrect_balance()
+    {
+        $balance = "abc";
+        $user = User::factory()->create();
+        $account = Account::create([
+            'name' => 'Source Test Account',
+            'current_balance' => $balance,
             'user_id' => $user->id
         ]);
-        Account::transfer($source_acc, $dest_acc, 20);
-        $this->assertEquals(220, $dest_acc->current_balance);
+        $this->assertEquals($balance, $account->current_balance);
     }
 }
