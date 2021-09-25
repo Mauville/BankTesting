203f42fcd1ef22f1156efba4725811d6a837ac9d
Jose Tlacuilo <tlacuilose@outlook.com>
2021-09-24 21:14:03 -0500

    Same user account movement unit test.

--- tests/Unit/AccountMovementTest.php
+++ tests/Unit/AccountMovementTest.php
@@ -2,17 +2,33 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Account;
+use App\Models\User;
 
 class AccountMovementTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_firstparty_transfer()
     {
-        $this->assertTrue(true);
+        $user = User::factory()->create();
+        $source_acc = Account::create([
+            'name' => 'Source Test Account',
+            'current_balance' => 100,
+            'user_id' => $user->id
+        ]);
+        $dest_acc = Account::create([
+            'name' => 'Destination Test Account',
+            'current_balance' => 200,
+            'user_id' => $user->id
+        ]);
+        Account::transfer($source_acc, $dest_acc, 20);
+        $this->assertEquals(220, $dest_acc->current_balance);
     }
 }
